12750e6839dd3507b5c8fa4f8504b85762292254
Cauã Cola Zandonadi <141333447+cauacz@users.noreply.github.com>
2025-01-30 11:56:26 -0300

    teste

---
+++ src/br/com/alura/screenmatch/principal/Listas.java
@@ -0,0 +1,41 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+
+public class Listas {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O Poderoso Chefão", 1970);
+        meuFilme.avaliar(9);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avaliar(6);
+        Filme filmeAlura = new Filme("Dogville", 2003);
+        filmeAlura.avaliar(10);
+        Serie serie = new Serie("Prison Break", 2005);
+        Filme filme3 = filmeAlura;
+
+        filme3.setNome("Nimguem te perguntou");
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(filmeAlura);
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(serie);
+
+        for(Titulo item: lista){ //foreach
+            System.out.println(item.getNome()); // Para cada item do tipo 'Titulo' da 'lista', serão representados
+
+            // Filme filme = (Filme) item; // Casting: todos item é um filme
+            //                   ou
+            if(item instanceof Filme filme){ // se item for uma instância de Filme
+                System.out.println("Classificação: " + filme.getClassificacao());
+            }
+
+
+        }
+
+    }
+}
